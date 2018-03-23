2e938b62f45ec2ef79ad4b828aee6253ad1b0f1c
Overlord_of_Materia <luccatavares11@gmail.com>
2018-03-23 17:20:09 +0000

    Feat : Make display work

--- app/src/main/java/com/example/android/qpcalculator/MainActivity.java
+++ app/src/main/java/com/example/android/qpcalculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.qpcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    int partA = 1;
+    int partB = 1;
+    int partC = 1;
+    int partD = 1;
+
+    // Display for result
+    public void quantity(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.result_text_view);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void resultClick(View view) {
+        int finalFlash = partA + partB + partC + partD;
+        quantity(finalFlash);
+    }
+
+
+
+
 }
