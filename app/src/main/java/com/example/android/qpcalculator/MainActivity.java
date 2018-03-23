9e08b96aace84cf0ce3d7a9c2669d2fc9ad3346b
Overlord_of_Materia <luccatavares11@gmail.com>
2018-03-23 17:38:47 +0000

    Feat : Add eqution code

--- app/src/main/java/com/example/android/qpcalculator/MainActivity.java
+++ app/src/main/java/com/example/android/qpcalculator/MainActivity.java
@@ -13,10 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int partA = 1;
-    int partB = 1;
-    int partC = 1;
-    int partD = 1;
+    int score1 = 1026;
+    int score2 = 1056;
+    int result1 = 24;
+    int result2 = 20;
 
     // Display for result
     public void quantity(int score) {
@@ -24,10 +24,17 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    // Set button behaviour
     public void resultClick(View view) {
-        int finalFlash = partA + partB + partC + partD;
-        quantity(finalFlash);
     }
+
+    // QP equation
+    public double qpEquation() {
+        double finalFlash =
+                (score1 * (0.1+((result1 - result2)/40)/10))-(score2*(0.1+((result2-result1)/40)/10));
+        return finalFlash;
+    }
+
 
 
 
